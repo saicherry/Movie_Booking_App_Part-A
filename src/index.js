9aa13b24ad3ec2d8ef8a21aa4e3256332703b59f
saicherry <saicherry196@gmail.com>
2021-11-18 20:43:43 +0530

    Checkpoint 1

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Home from "./screens/home/Home.js";
+
+ReactDOM.render(
+    <div>
+  <Home/>
+    </div>,
+
+  document.getElementById('root')
+);
+
+
