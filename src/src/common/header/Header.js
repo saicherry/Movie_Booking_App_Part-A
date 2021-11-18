267d11b9f8bf7e3d8ad0e5c081a5f42c7d75c3b0
saicherry <saicherry196@gmail.com>
2021-11-18 20:51:55 +0530

    Checkpoint 5

---
+++ src/src/common/header/Header.js
@@ -0,0 +1,17 @@
+import react from "react";
+import "./header.css";
+
+import logo from "./logo.svg";
+
+class Header extends react.Component{
+
+    render(){
+        return(
+            <div className="header">
+            <img src={logo} alt="logo" id="image"></img>
+            </div>
+        )
+    }
+}
+
+export default Header;
